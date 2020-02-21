0cd180f7766d5baee83cc61d538a924f7c13e505
hiroyuki12 <hiroyuki12@gmail.com>
2020-02-21 22:51:21 +0900

    add DetailScreen Navigator

--- lib/pages/CupertinoScreen.dart
+++ lib/pages/CupertinoScreen.dart
@@ -31,15 +31,45 @@ class _State extends State<CupertinoScreen> {
                 middle: (i == 0) ? Text('Articles') : Text('Views'),
               ),
               child: Center(
-                child: Text(
-                  'This is tab #$i',
-                  style: CupertinoTheme.of(context).textTheme.navLargeTitleTextStyle,
+                child: CupertinoButton(
+                  child: Text(
+                    'This is tab #$i',
+                    style: CupertinoTheme.of(context)
+                      .textTheme
+                      .actionTextStyle
+                      .copyWith(fontSize: 32),
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      CupertinoPageRoute(builder: (context) {
+                        return DetailScreen(i == 0 ? 'Articles' : 'Views');
+                      }),
+                    );
+                  },
                 ),
               ),
             );
           }
         );
       }
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen(this.topic);
+  final String topic;
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('Detail'),
+      ),
+      child: Center(
+        child: Text('Details for $topic',
+        style: CupertinoTheme.of(context).textTheme.navLargeTitleTextStyle)
+      )
     );
   }
 }
