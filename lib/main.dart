d56c2a6aac27e838a05c5ac88838b03f80af22b0
hiroyuki12 <hiroyuki12@gmail.com>
2020-02-22 01:07:23 +0900

    add LoremPicsumScreen

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ import 'pages/HelloWorld.dart';
 import 'pages/LayoutScreen.dart';
 import 'pages/ListView2Screen.dart';
 import 'pages/ListViewScreen.dart';
+import 'pages/LoremPicsumScreen.dart';
 import 'pages/NavigatorScreen.dart';
 import 'pages/ProviderScreen.dart';
 import 'pages/SettingScreen.dart';
@@ -124,7 +125,7 @@ class _MyHomePageState extends State<MyHomePage> {
         children: [
           HelloWorld(),
           SettingScreen(),
-          MyCupertinoActionSheet(),
+          LoremPicsum(),
         ]),
         // ページ下部のナビゲーションメニュー
         bottomNavigationBar: BottomNavigationBar(
---
+++ lib/pages/LoremPicsumScreen.dart
@@ -0,0 +1,67 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class LoremPicsum extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _State();
+  }
+}
+
+class Item {
+   Item({
+     this.author,
+     this.download_url,
+   });
+
+   final String author;
+   final String download_url;
+ }
+
+class _State extends State<LoremPicsum> {
+  List<Item> _items = <Item>[];
+  @override
+  void initState() {
+    super.initState();
+    _load();
+  }
+  
+  // This widget is the root of your application.
+  Future<void> _load() async {
+    final res = await http.get('https://picsum.photos/v2/list?page=2&limit=3');
+    final data = json.decode(res.body);
+    setState(() {
+      final issues = data as List;
+      issues.forEach((dynamic element) {
+        final item = element as Map;
+        _items.add(Item(
+          author: item['author'] as String,
+          download_url: item['download_url'] as String,
+        ));
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lorem Picsum"),
+      ),
+      body: ListView.builder(
+        itemBuilder: (BuildContext context, int index) {
+          if (index >= _items.length) {
+            return null;
+          }
+
+          final item = _items[index];
+          return ListTile(
+            leading: Image.network(item.download_url),
+            title: Text(item.author),
+          );
+        },
+      ),
+    );
+  }
+}
