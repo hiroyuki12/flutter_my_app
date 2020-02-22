30ecdcc6d0cf3d3ce2ac53bfae94e49f771f1c63
hiroyuki12 <hiroyuki12@gmail.com>
2020-02-22 21:55:06 +0900

    add BottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'pages/BottomNavigationBar.dart';
 import 'pages/CounterScreen.dart';
 import 'pages/Cupertino/CupertinoScreen.dart';
 import 'pages/Cupertino/CupertinoTabBarScreen.dart';
@@ -48,102 +49,8 @@ class MyApp extends StatelessWidget {
         '/cupertinoTabBar': (context) => CupertinoTabBarScreen(),
         '/home': (context) => MyHome(),
       },
-      //home: MyHomePage(title: 'main'),  //BottomNavigationBar
-      home: MyHome(),                     //CupertinoTabBar (Bottom)
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  // ページ切り替え用のコントローラを定義
-  PageController _pageController;
-  // ページインデックス保存用
-  int _screen = 0;
-  // ページ下部に並べるナビゲーションメニューの一覧
-  List<BottomNavigationBarItem> myBottomNavBarItems() {
-    return [
-      BottomNavigationBarItem(
-        icon: Icon(Icons.home),
-        title: const Text('HelloWorld'),
-      ),
-      BottomNavigationBarItem(
-        icon: Icon(Icons.settings),
-        title: const Text('Setting'),
-      ),
-      BottomNavigationBarItem(
-        icon: Icon(Icons.repeat),
-        title: const Text('ListViewScreen'),
-      ),
-    ];
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    // コントローラ作成
-    _pageController = PageController(
-      initialPage: _screen, // 初期ページの指定。上記で_screenを１とすれば２番目のページが初期表示される。
-    );
-  }
-/*
-  @override
-  void dispose() {
-    // コントローラ破棄
-    _pageController.dispose();
-    super.dispose();
-  }
-*/
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      
-      body: PageView(
-        controller: _pageController,
-        // ページ切り替え時に実行する処理
-        // PageViewのonPageChangedはページインデックスを受け取る
-        // 以下ではページインデックスをindexとする
-        onPageChanged: (index) {
-          setState(() {
-            // ページインデックスを更新
-            _screen = index;
-          });
-        },
-        // ページ下部のナビゲーションメニューに相当する各ページビュー。後述
-        children: [
-          HelloWorld(),
-          Setting(),
-          MyHome(),
-        ]),
-        // ページ下部のナビゲーションメニュー
-        bottomNavigationBar: BottomNavigationBar(
-        // 現在のページインデックス
-        currentIndex: _screen,
-        // onTapでナビゲーションメニューがタップされた時の処理を定義
-        onTap: (index) {
-          setState(() {
-            // ページインデックスを更新
-            _screen = index;
-
-            // ページ遷移を定義。
-            // curveで指定できるのは以下
-            // https://api.flutter.dev/flutter/animation/Curves-class.html
-            //_pageController.jumpToPage(index);
-            _pageController.animateToPage(index,
-                duration: Duration(milliseconds: 300), curve: Curves.easeOut);
-          });
-        },
-        // 定義済のナビゲーションメニューのアイテムリスト
-        items: myBottomNavBarItems(),
-      ),
+      home: MyBottomNavigationBar(title: 'main'),  //BottomNavigationBar
+      //home: MyHome(),                     //CupertinoTabBar (Bottom)
     );
   }
 }
