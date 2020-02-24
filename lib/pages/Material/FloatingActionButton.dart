e4a99ecefd82079f02d4a962f5abd1060b823982
hiroyuki12 <hiroyuki12@gmail.com>
2020-02-24 15:55:47 +0900

    add FloatingActionButton

--- lib/pages/Material/FloatingActionButton.dart
+++ lib/pages/Material/FloatingActionButton.dart
@@ -15,9 +15,22 @@ class _State extends State<MyFloatingActionButton> {
       appBar: AppBar(
         title: Text("FloatingActionButton"),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () => setState(() => _count++),
+      floatingActionButton: Column(
+        verticalDirection: VerticalDirection.up,  //下に配置
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () => setState(() => _count++),
+          ),
+          Container(  //余白のため
+            margin: EdgeInsets.only(bottom: 16.0),
+            child: FloatingActionButton(
+              child: Icon(Icons.remove),
+              onPressed: () => setState(() => _count--),
+            ),
+          ),
+        ],
       ),
       body: Center(
         child: Text(_count.toString(),),
