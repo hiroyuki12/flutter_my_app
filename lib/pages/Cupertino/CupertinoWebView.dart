a22c2cf00ac22c430e7a3306b829d9e98b64f15c
hiroyuki12 <hiroyuki12@gmail.com>
2020-03-11 22:28:45 +0900

    add Share CupertinoWebView.

--- lib/pages/Cupertino/CupertinoWebView.dart
+++ lib/pages/Cupertino/CupertinoWebView.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:share/share.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'DarkModeColor.dart';
 
@@ -13,7 +15,6 @@ class MyCupertinoWebView extends StatefulWidget {
   }
 }
 
-
 class _State extends State<MyCupertinoWebView> {
   WebViewController _controller;
 
@@ -21,29 +22,20 @@ class _State extends State<MyCupertinoWebView> {
   Widget build(BuildContext context) {
     isDarkMode = true;  // switch darkMode
     return CupertinoPageScaffold(
-      backgroundColor: isDarkMode ? darkModeBackColor : backColor,  //white , darkMode=black
+      // backgroundColor: isDarkMode ? darkModeBackColor : backColor,  //white , darkMode=black
       navigationBar: CupertinoNavigationBar(
-        // leading: CupertinoButton(
-        //   onPressed: () {
-        //     // _controller.loadUrl('https://youtube.com/');
-        //     _controller.loadUrl('https://flutter.dev/');
-        //   },
-        //   child: Text('Flutter', style: _buildTextStyle()),
-        // ),
         middle: Text("CupertinoWebView", style: _buildTextStyle()),
-        trailing: CupertinoButton(
-          onPressed: () {
-            _controller.loadUrl('https://flutter.dev/');
-            // _controller.loadUrl(widget.url);
-          },
-          child: Text('Flutter', style: _buildTextStyle()),
+        trailing: GestureDetector(
+          onTap: () => Share.share(widget.url),
+          child: Icon(
+            CupertinoIcons.share,
+            color: CupertinoColors.systemGrey,
+          ),
         ),
-        // backgroundColor: const Color(0xff333333),
         backgroundColor: isDarkMode ? darkModeBackColor : backColor,  //white , darkMode=black
       ),
       child: Center(
         child: WebView(
-          // initialUrl: 'https://youtube.com/',
           initialUrl: widget.url,
           javascriptMode: JavascriptMode.unrestricted,
           onWebViewCreated: (WebViewController controller) {
